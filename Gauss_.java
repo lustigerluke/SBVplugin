779274e7e74d91efe9e00628803cde524f8100d5
Unknown <lustiger.luke@gmail.com>
2018-10-21 15:20:23 +0200

    renew Gauss Doku

--- Gauss_.java
+++ Gauss_.java
@@ -1,36 +1,34 @@
 import ij.*;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.*;
-
 import ij.gui.GenericDialog;
 
 public class Gauss_ implements PlugInFilter {
-  
-   public int setup(String arg, ImagePlus imp) {
-		if (arg.equals("about"))
-			{showAbout(); return DONE;}
-		return DOES_8G+DOES_STACKS+SUPPORTS_MASKING;
-	} //setup
-	
 
-	
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about")) {
+			showAbout();
+			return DONE;
+		}
+		return DOES_8G + DOES_STACKS + SUPPORTS_MASKING;
+	} // setup
+
 	public void run(ImageProcessor ip) {
 		int width = ip.getWidth();
 		int height = ip.getHeight();
 		int tgtRadius = getUserInput(4, "radius");
 		int sigma = getUserInput(4, "sigma");
-		
+
 		double[][] resultImage = runFilter(ip, tgtRadius, sigma);
-		
+
 		ImageJUtility.showNewImage(resultImage, width, height, "mean with kernel r=" + tgtRadius);
-                        
-	} //run
+
+	} // run
 
 	void showAbout() {
-		IJ.showMessage("About Template_...",
-			"this is a PluginFilter template\n");
-	} //showAbout
-	
+		IJ.showMessage("About Template_...", "this is a PluginFilter template\n");
+	} // showAbout
+
 	/**
 	 * Asks the user to input.
 	 * 
@@ -49,22 +47,20 @@ public class Gauss_ implements PlugInFilter {
 		System.out.println(radius);
 		return radius;
 	}
-	
+
 	public static double[][] runFilter(ImageProcessor ip, int radius, int sigma) {
 		// convert to pixel array
-		byte[] pixels = (byte[])ip.getPixels();
+		byte[] pixels = (byte[]) ip.getPixels();
 		int width = ip.getWidth();
 		int height = ip.getHeight();
 		int tgtRadius = radius;
-		
+
 		int[][] inArr = ImageJUtility.convertFrom1DByteArr(pixels, width, height);
 		double[][] inDataArrDouble = ImageJUtility.convertToDoubleArr2D(inArr, width, height);
-		
-		double[][] filterMask = ConvolutionFilter.GetGaussMask(tgtRadius,sigma);
+
+		double[][] filterMask = ConvolutionFilter.GetGaussMask(tgtRadius, sigma);
 		return ConvolutionFilter.ConvolveDoubleNorm(inDataArrDouble, width, height, filterMask, tgtRadius);
-        
+
 	}
-	
-} //class FilterTemplate_
 
-
+} // class FilterTemplate_
