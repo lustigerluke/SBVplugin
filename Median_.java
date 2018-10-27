44bc9f298f1e693806d7340d2a7bdd31637f6ffe
Unknown <lustiger.luke@gmail.com>
2018-10-27 15:03:02 +0200

    plot 4x4 image: done

--- Median_.java
+++ Median_.java
@@ -36,8 +36,49 @@ public class Median_ implements PlugInFilter {
 		System.out.println("Now show the result image!");
 		ImageJUtility.showNewImage(resultImage, width, height, "mean with kernel r=" + radius);
 		System.out.println("SUCCESS: MEDIAN FILTER DONE.");
+		
+		System.out.println("Now plot 4x4 to see filtereffect.");
+
+		plot4x4(ip, resultImage);
 
 	} // run
+
+	private void plot4x4(ImageProcessor ip, double[][] filteredImg) {
+		int segments = 4;
+		
+		byte[] pixels = (byte[]) ip.getPixels();
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		int[][] inArr = ImageJUtility.convertFrom1DByteArr(pixels, width, height);
+		double[][] resultImg = ImageJUtility.convertToDoubleArr2D(inArr, width, height);
+
+		
+		int xCaroLength = width / segments;
+		int yCaroLength = height / segments;
+		
+		// for every region
+		for (int i = 0; i < segments; i++) {
+			for (int j = 0; j < segments; j++) {
+				if ((i+j) % 2 == 0) {
+					int xIndex = i*xCaroLength;
+					int yIndex = j*yCaroLength;
+					// calculate region
+					Rectangle roi = new Rectangle(xIndex, yIndex, xCaroLength, yCaroLength);
+					double[][] tmpImg = ImageJUtility.cropImage(filteredImg, roi.width, roi.height, roi);
+					
+					// copy region to result image
+					for (int x = 0; x < xCaroLength; x++) {
+						for (int y = 0; y < yCaroLength; y++) {
+							resultImg[xIndex + x][yIndex + y] =  tmpImg[x][y];
+						}
+					}			
+				}
+			}
+		}
+		
+		ImageJUtility.showNewImage(resultImg, width, height, "4x4 caro for filter effect evaluation");
+		
+	}
 
 	public static double[][] runFilter(ImageProcessor ip, int radius) {
 		byte[] pixels = (byte[]) ip.getPixels();
