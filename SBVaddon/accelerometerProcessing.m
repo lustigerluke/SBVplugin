830716ea2755d86dc589ff14ab9829a8c1c15c1a
Unknown <lustiger.luke@gmail.com>
2018-10-30 23:31:05 +0100

    added first plots
    read xls needs io package from octave
    review axis

--- SBVaddon/accelerometerProcessing.m
+++ SBVaddon/accelerometerProcessing.m
@@ -12,4 +12,28 @@ filePath = [folderPath, fileName]
 
 A = xlsread(filePath, 'tisch2m', 'A2:B577');
 
-plot(A(:,1),A(:,2));
+B(1) = 0;
+N = length(A(:,1));
+for j=2:N
+  B(j) = A(j,2) + B(j-1);   
+endfor
+
+
+C(1) = 0;
+for j=2:N
+  C(j) = B(j) + C(j-1);   
+endfor
+
+
+
+figure()
+subplot(2,1,1);
+xlabel("time in ms");
+ylabel("acc in m/s^2");
+hold on
+plot(A(:,1),A(:,2),"-b;acceleration;");
+plot(A(:,1),B,"-;velocity;")
+subplot(2,1,2);
+xlabel("time in ms");
+ylabel("way");
+plot(A(:,1),C,"-b;distance;")
