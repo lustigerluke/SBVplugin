4f515ecccea2cf708cf2f8be475b48ca9ace20f2
Unknown <lustiger.luke@gmail.com>
2018-11-01 10:32:07 +0100

    plot plain accelerometer data
    acc
    vel
    way

--- SBVaddon/accelerometerProcessing.m
+++ SBVaddon/accelerometerProcessing.m
@@ -6,34 +6,38 @@ close all
 pkg load io
 
 'load files from '
-folderPath = 'C:/Users/Lukas/Documents/Signal- und Bildverarbeitung/workspace/project/plugins/SBVplugin/SBVaddon/testData/'
+folderPath = 'C:/Users/Lukas/Documents/Signal- und Bildverarbeitung/workspace/project/plugins/SBVplugin/SBVaddon/testData/';
 fileName = 'acceleration.csv';
 filePath = [folderPath, fileName]
 
-A = xlsread(filePath, 'tisch2m', 'A2:B577');
+acceleration = xlsread(filePath, 'tisch2m', 'A2:B577');
 
-B(1) = 0;
-N = length(A(:,1));
+'process plain data'
+velocity(1) = 0; # assume the velocity to be zero at the beginning
+N = length(acceleration(:,1));
 for j=2:N
-  B(j) = A(j,2) + B(j-1);   
+  velocity(j) = acceleration(j,2) + velocity(j-1);   
 endfor
 
-
-C(1) = 0;
+way(1) = 0; # assume the way to be zero at the beginning
 for j=2:N
-  C(j) = B(j) + C(j-1);   
+  way(j) = velocity(j) + way(j-1);   
 endfor
 
-
-
+'plot plain data'
 figure()
 subplot(2,1,1);
 xlabel("time in ms");
-ylabel("acc in m/s^2");
+ylabel("acceleration, velocity");
 hold on
-plot(A(:,1),A(:,2),"-b;acceleration;");
-plot(A(:,1),B,"-;velocity;")
+plot(acceleration(:,1),acceleration(:,2),"-b;acceleration in m/s^2;");
+plot(acceleration(:,1),velocity,"-;velocity in m/2;")
 subplot(2,1,2);
 xlabel("time in ms");
 ylabel("way");
-plot(A(:,1),C,"-b;distance;")
+hold on
+plot(acceleration(:,1),way,"-b;distance;")
+
+# the way displayed is decreasing at the end but should be constant
+# if the velocity would be zero, the way would be constant
+
