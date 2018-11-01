caaf58bef8ca268630c531bfc565dd23990be35d
Unknown <lustiger.luke@gmail.com>
2018-11-01 20:04:45 +0100

    good results

--- SBVaddon/accelerometerProcessing.m
+++ SBVaddon/accelerometerProcessing.m
@@ -25,19 +25,86 @@ for j=2:N
 endfor
 
 'plot plain data'
-figure()
+fig1 = figure();
 subplot(2,1,1);
 xlabel("time in ms");
 ylabel("acceleration, velocity");
 hold on
-plot(acceleration(:,1),acceleration(:,2),"-b;acceleration in m/s^2;");
-plot(acceleration(:,1),velocity,"-;velocity in m/2;")
+plot(acceleration(:,1),acceleration(:,2),"-r;acceleration in m/s^2;");
+plot(acceleration(:,1),velocity,"-g;velocity in m/2;")
 subplot(2,1,2);
 xlabel("time in ms");
 ylabel("way");
 hold on
 plot(acceleration(:,1),way,"-b;distance;")
 
-# the way displayed is decreasing at the end but should be constant
-# if the velocity would be zero, the way would be constant
 
+'begin CORRECTION'
+# ACCELERATION CORRECTION#############################################################################
+time = acceleration(:,1);
+correctedAcceleration = acceleration(:,2);
+# ACCELERATION - floating mean filter
+radius = 2;
+mask = 2 * radius +1;
+for j = (radius+1) : N- (radius+1)
+  correctedAcceleration(j) = mean(correctedAcceleration(j-radius:j+radius));   
+endfor
+
+# ACCELERATION - threshold
+THRESHOLD = 0.12;
+for j = (radius+1) : N - (radius+1)
+  meanValue = abs(mean(correctedAcceleration(j-radius:j+radius)));
+  if (meanValue < THRESHOLD)
+    correctedAcceleration(j) = 0;   
+  endif
+endfor
+correctedAcceleration(1:radius) = correctedAcceleration(radius+1);
+correctedAcceleration(N-radius:N) = correctedAcceleration(N-radius-1);
+
+# VELOCITY CALCULATION
+correctedVelocity(1) = 0; # assume the velocity to be zero at the beginning
+N = length(time);
+for j=2:N
+  correctedVelocity(j) = correctedAcceleration(j) + correctedVelocity(j-1);   
+endfor
+
+# VELOCITY CORRECTION
+
+# find indizes of actual movement in accelerator data
+nonzeroIndizes = find(correctedAcceleration(:)); # find nonzero data
+lastPrecedentZeroIndex = min(nonzeroIndizes) # get first nonzero index
+lastNonZeroIndex = max(nonzeroIndizes) # get last nonzero index
+
+# calculate line from first meaningful data to the last
+k = correctedVelocity(length(correctedVelocity))/(lastNonZeroIndex - lastPrecedentZeroIndex);
+d = - k * lastPrecedentZeroIndex;
+correctedVelocity(1:lastPrecedentZeroIndex) = 0; # if there is no acceleration -> set velocity to 0
+correctedVelocity(lastNonZeroIndex:N) = 0; # if ther is no acceleration -> set velocity to 0
+for j=lastPrecedentZeroIndex:lastNonZeroIndex-1
+  correctedVelocity(j) = correctedVelocity(j) + abs(k * j + d);   
+endfor
+
+
+correctedWay(1) = 0; # assume the way to be zero at the beginning
+for j=2:N
+  correctedWay(j) = correctedVelocity(j) + correctedWay(j-1);   
+endfor
+
+
+'plot result'
+distance = max(way) 
+correctedDistance = max(correctedWay)
+
+figure()
+subplot(2,1,1)
+grid on
+hold on
+plot(time,acceleration(:,2),"-r;acceleration;");
+plot(time,correctedAcceleration,"-b;corrected acceleration;");
+plot(time,correctedVelocity(:),"-c;corrected velocity;");
+subplot(2,1,2)
+hold on
+grid on
+plot(time,way,"-c;distance;");
+plot(time,correctedWay(:),"-r;corrected distance;");
+legend("location","southeast");
