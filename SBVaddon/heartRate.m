5dc107d97c4a1d55956f9da5d23d8a69ae21c6c5
Unknown <lustiger.luke@gmail.com>
2018-11-05 23:20:56 +0100

    bad state
    trying to find main freqhency

--- SBVaddon/heartRate.m
+++ SBVaddon/heartRate.m
@@ -13,15 +13,32 @@ for i=1:numel(files)
   [~, name] = fileparts (files{i});
   ## eval(sprintf('%s = load("%s", "-ascii");', name, files{i}));
   
-  acceleration = load(files{i});
-
-  'plot plain data'
+  data = load(files{i}); #load data
+  # remove header rows
+  tInd = data(4:length(data),1); #time index
+  plainSignal = data(4:length(data),3);
+  
+  #calculate resonanz to get an idea of the heart rate. 
+  fftData = fft(plainSignal);
+  find(max(fftData(5:length(fftData)-5)))
+  
+  fftTime = tInd(1:1000);
+  fftData = fftData(1:1000);
+  sampling_frequency = 1000;
+  fftFreq(i) =(i-1)*sampling_frequency/length(fftData);
+  
   fig1 = figure();
-  hold on;
+  subplot(2,1,1);hold on; # first subplot
   title(name)
   xlabel("time in ms");
   ylabel("ch1");
   grid on
-  plot(acceleration(:,1),acceleration(:,3),"-b;plain data;");
+  plot(tInd,plainSignal,"-b;plain data;");
+  subplot(2,1,2);hold on; # second subplot
+  xlabel("amp");
+  ylabel("ch1");
+  grid on
+  plot(fftFreq,fftData,"-b;fft;");
+
 
 end
