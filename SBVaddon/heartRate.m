ec9023cedd77d9c8bef1c8a27ab1e2dba1e61087
Unknown <lustiger.luke@gmail.com>
2018-11-06 23:20:03 +0100

    try to fft

--- SBVaddon/heartRate.m
+++ SBVaddon/heartRate.m
@@ -7,41 +7,56 @@ pkg load financial
 
 'load files from '
 folderPath = 'C:/Users/Lukas/Documents/Signal- und Bildverarbeitung/workspace/project/plugins/SBVplugin/SBVaddon/testData/*.txt'
-sampling_frequnecy = 10;
-
+SAMPLING_FREQUENCY = 1000;
 
 files = glob(folderPath)
-for i=1:2 # for debugging
-#for i=1:numel(files)
+#for i=1:2 # for debugging
+for i=1:numel(files)
   [~, name] = fileparts (files{i});
   ## eval(sprintf('%s = load("%s", "-ascii");', name, files{i}));
   
+  # read file
   data = load(files{i}); #load data
+  
   # remove header rows
-  tInd = data(4:length(data),1); #time index
+  tIndex = data(4:length(data),1); #time in ms
+  t = (1/SAMPLING_FREQUENCY)*(1:length(tIndex)); # time in s
   plainSignal = data(4:length(data),3);
   
-  #calculate mean
+  # moving average to remove offset
   wndw = length(plainSignal)/10;            %# sliding window size
   meanVec = movavg(plainSignal,wndw-1,wndw);%# moving average
-
   resultSignal = plainSignal - meanVec;
   
+  # moving average as a filter
+  wndw = 2;            %# sliding window size
+  resultSignal = movavg(resultSignal,wndw-1,wndw);%# moving average
   
-  
+  #calculate resonanz to get an idea of the heart rate.  
+  y_fft = abs(fft(resultSignal));
+  y_fft = y_fft(1:length(y_fft)/4); % discard useless points
+  f = SAMPLING_FREQUENCY * (0:length(resultSignal)/4-1)/length(resultSignal);
+  # remove first data as 0 is main frequency
+
   
   fig1 = figure();
-  subplot(2,1,1);hold on; # first subplot
+  subplot(2,2,1);hold on; # first subplot
   title(name)
   xlabel("time in ms");
   ylabel("ch1");
   grid on
-  plot(tInd,plainSignal,"-b;plain data;");
-  subplot(2,1,2);hold on; # second subplot
+  plot(tIndex,plainSignal,"-b;plain data;");
+  subplot(2,2,2);hold on; # second subplot
   xlabel("amp");
-  ylabel("ch1");
+  ylabel("time in s");
   grid on
-  plot(tInd,resultSignal,"-b;filtered data;");
+  plot(t,resultSignal,"-b;filtered data;");
 
+  subplot(2,2,3);hold on; # second subplot
+  xlabel("frequency");
+  ylabel("amplitude");
+  grid on
+  plot(f,y_fft)
 
+  
 end
