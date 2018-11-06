58efa607fbc3f6a87db6f977027e7f533fd29c39
Unknown <lustiger.luke@gmail.com>
2018-11-06 19:57:08 +0100

    remove offset of signal

--- SBVaddon/heartRate.m
+++ SBVaddon/heartRate.m
@@ -3,13 +3,16 @@ clc
 clear all
 close all
 
+pkg load financial
+
 'load files from '
 folderPath = 'C:/Users/Lukas/Documents/Signal- und Bildverarbeitung/workspace/project/plugins/SBVplugin/SBVaddon/testData/*.txt'
 sampling_frequnecy = 10;
 
 
 files = glob(folderPath)
-for i=1:numel(files)
+for i=1:2 # for debugging
+#for i=1:numel(files)
   [~, name] = fileparts (files{i});
   ## eval(sprintf('%s = load("%s", "-ascii");', name, files{i}));
   
@@ -18,14 +21,12 @@ for i=1:numel(files)
   tInd = data(4:length(data),1); #time index
   plainSignal = data(4:length(data),3);
   
-  #calculate resonanz to get an idea of the heart rate. 
-  fftData = fft(plainSignal);
-  find(max(fftData(5:length(fftData)-5)))
+  #calculate mean
+  wndw = length(plainSignal)/10;                                      %# sliding window size
+  #meanVec = filter(ones(wndw,1)/wndw, 1, plainSignal); %# moving average
+  meanVec = movavg(plainSignal,wndw-1,wndw);
   
-  fftTime = tInd(1:1000);
-  fftData = fftData(1:1000);
-  sampling_frequency = 1000;
-  fftFreq(i) =(i-1)*sampling_frequency/length(fftData);
+  resultSignal = plainSignal - meanVec;
   
   fig1 = figure();
   subplot(2,1,1);hold on; # first subplot
@@ -38,7 +39,7 @@ for i=1:numel(files)
   xlabel("amp");
   ylabel("ch1");
   grid on
-  plot(fftFreq,fftData,"-b;fft;");
+  plot(tInd,resultSignal,"-b;filtered data;");
 
 
 end
