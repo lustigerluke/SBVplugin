4bc923c9ddca1bf848a803944ae9c257df48d51b
Unknown <lustiger.luke@gmail.com>
2018-11-07 22:07:26 +0100

    first calculation

---
+++ SBV2/calculateZipMatrix.m
@@ -0,0 +1,23 @@
+
+clc
+clear all
+close all
+
+'load files from '
+# constants
+# please note if copy-pasting windows paths: there are no backslashes int he path 
+folderPath = 'C:/Users/Lukas/Documents/Signal- und Bildverarbeitung/workspace/project/plugins/SBVplugin/SBV2/testData/'
+sampling_frequnecy = 10;
+
+searchPath = [folderPath , '*']
+files = glob(searchPath)
+for i=2:numel(files)
+  'get current file name'
+  [~, name] = fileparts (files{i})
+  files{i}
+  cd(folderPath);
+  zip('1.zip',files{i})
+
+endfor
+
+'SUCCESS'
